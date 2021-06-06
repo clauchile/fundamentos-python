f5970379fe64eec7b7ed56ddcb09c093d117a606
Claudio <clauchile@gmail.com>
2021-06-06 12:38:31 -0400

    primer commit

---
+++ hello_world.py
@@ -0,0 +1,24 @@
+print(f"Hola mundo")
+
+name = "Claudio"
+print("hola", name)
+#Almacena tu número favorito en una variable, y luego úsalo para imprimir la cadena "¡Hola {{num}}!" usando una coma en la función de impresión (# 3a)
+num = 21
+print("Hola" , num)
+#Almacena tu número favorito en una variable, y luego úsalo para imprimir la cadena "¡Hola {{num}}!" usando un + en la función de impresión (# 3b)
+num = "21"
+print ("Hola " + num)
+# Almacena 2 de tus comidas favoritas en variables y luego úsalas para imprimir 
+# la cadena "Me encanta comer {{food_one}} y {{food_two}}". con el método de formato (# 4a)
+fave_food1 = "hamburgesas"
+fave_food2 = "pizza"
+
+print (f"Me encanta comer {fave_food1} y {fave_food2}.") # con una cadena f
+
+
+# Almacena 2 de tus comidas favoritas en variables y luego úsalas para imprimir la cadena 
+# "Me encanta comer {{food_one}} y {{food_two}}". con cadenas f (# 4b)
+fave_food1 = "hamburgesas"
+fave_food2 = "pizza"
+
+print (f"Me encanta comer {fave_food1} y {fave_food2}.") # con una cadena f
